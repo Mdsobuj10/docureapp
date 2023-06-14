009f32a795f0649580cc637bb73fd2a1f9170346
sobuj mia <mdsobujmia287@gmail.com>
2023-06-14 12:43:37 +0600

    all file setup

---
+++ app/Http/Controllers/FrontendController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FrontendController extends Controller
+{
+   /* 
+   show home page */
+    public function index()
+    {
+        return view('frontend.home');
+    }
+}
